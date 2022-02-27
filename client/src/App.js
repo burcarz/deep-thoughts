b18e4628066cb563f3fea4ab833402a01effe394
burcar <zacharyburcar@gmail.com>
2022-02-27 15:30:42 -0500

    added react

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ApolloProvider, AplloClient, InMemoryCache, createHttpLink, ApolloClient } from '@apollo/client';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <div className='flex-column justify-flex-start min-100-vh'>
+      <Header />
+      <div className='container'>
+        <Home />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,30 @@
 const express = require('express');
+// import apolloserver
+const { ApolloServer } = require('apollo-server-express');
+// import typedefs and resolvers
+const { typeDefs, resolvers } = require('./schema');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const startServer = async() => {
+  // create new apollo server
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
+  // start apollo server
+  await server.start();
+  // integrate apollo server with express app as middleware
+  server.applyMiddleware({ app });
+  // log test url
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+// init apollo server
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
